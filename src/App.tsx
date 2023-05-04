e4059559b5baabd958b8a0e20beffd05259be73a
webgently <oleksandrshkabura811@gmail.com>
2023-05-05 01:13:12 +0300

    update: app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
+import GameManager from './pages/GameManager';
 import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <GameManager />;
 }
 
 export default App;
