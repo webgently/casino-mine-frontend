d0eaaa405a52b8e49ef98b81e1fc350dd209f583
webgently <oleksandrshkabura811@gmail.com>
2023-05-05 04:55:04 +0300

    feat: implement the redux structure

---
+++ src/@types/global.d.ts
@@ -0,0 +1,19 @@
+declare module '.jpg';
+declare module '.svg';
+declare module '.png';
+declare module 'url:*' {
+  export default string;
+}
+declare module '*.svg' {
+  const content: any;
+  export default content;
+}
+declare interface StoreObject {
+  auth?: {
+    _id: string;
+    name: string;
+    img: string;
+    balance: number;
+  };
+  loading: boolean;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import GameManager from './pages/GameManager';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import { slice } from './useStore';
 import './App.scss';
 
+const store = configureStore({ reducer: slice.reducer });
 function App() {
-  return <GameManager />;
+  return (
+    <Provider store={store}>
+      <GameManager />
+    </Provider>
+  );
 }
 
 export default App;
