8d07b9c404b37ee2f52a84f685898fa01442f779
webgently <oleksandrshkabura811@gmail.com>
2023-05-09 12:40:36 +0300

    update: bet button component

--- src/components/BetButton/index.tsx
+++ src/components/BetButton/index.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import CricketImg from '../../assets/images/cricket.png';
+import CricketShadowImg from '../../assets/images/shadow.webp';
 import './betbutton.scss';
 
-const BetButton = ({ type }: any) => {
+const BetButton = ({ cardLoading, loading, type, turbo }: any) => {
   return (
-    <div className={`bet-button-${type}`}>
+    <div className={`bet-button-${type} ${(loading || cardLoading) && 'pointer-events-none'}`}>
       <div className="bet-button-inner">
         <div className="bet-button-content">
           <div className="bet-button-text">
-            {type === 'cancel' ? 'Cancel' : type === 'cashout' ? 'Cash Out' : 'Start Game'}
+            {loading ? (
+              <div className="loading-container">
+                <img src={CricketShadowImg} alt="shadow" />
+                <div className="btn-loading">
+                  <img src={CricketImg} alt="loading..." />
+                </div>
+              </div>
+            ) : type === 'cancel' ? (
+              'Cancel'
+            ) : type === 'cashOut' ? (
+              'Cash Out'
+            ) : (
+              `${turbo ? 'Place bet' : 'Start Game'}`
+            )}
           </div>
-          {type === 'cashout' && (
+          {type === 'cashOut' && (
             <div className="bet-button-sum">
               <div className="bet-button-sum-content">
                 <span>$10</span>
               </div>
             </div>
           )}
-          <div className="btn-loading">
-            <img src={CricketImg} alt="loading..." />
-          </div>
         </div>
       </div>
     </div>
