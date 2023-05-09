aa5e719c3670b3dd937e6f48e1b0bfb85d637ab1
webgently <oleksandrshkabura811@gmail.com>
2023-05-09 12:42:20 +0300

    update: card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 import './card.scss';
 
-const Card = () => {
+const Card = ({ active, turbo, mine, playStatus, self, currentTarget, loading, turboMode, turboList }: any) => {
   return (
-    <div className="play-target-item">
-      <div className="target-item-inner">
-        <div className="target-item-sum">$0.10</div>
+    <div
+      className={`play-target-item ${
+        (turboMode && !active) || (!active && playStatus) ? '_hover' : ' pointer-events-none'
+      }`}
+    >
+      <div
+        className={`target-item-inner ${!turboMode && !playStatus && 'before:opacity-[0.5]'} ${
+          turboMode
+            ? `${turbo && '_turbo_active'} ${
+                active
+                  ? mine
+                    ? `_lose ${turboList.indexOf(self) >= 0 ? 'opacity-[1]' : 'opacity-[0.5]'}`
+                    : '_win1'
+                  : turboList.indexOf(self) >= 0 && '_turbo_active'
+              } ${loading && turboList.indexOf(self) >= 0 && '_loading'}`
+            : loading && currentTarget === self
+            ? '_loading'
+            : active && (mine ? `_lose ${currentTarget === self ? 'opacity-[1]' : 'opacity-[0.5]'}` : '_win1')
+        }`}
+      >
+        {turboMode ? (
+          !active && turboList.indexOf(self) >= 0 && <span className="turbo-mark">?</span>
+        ) : (
+          <span className="target-item-sum">$0.10</span>
+        )}
       </div>
-      <div className="target-item-shadow" />
+      {!active && <div className="_shadow" />}
     </div>
   );
 };
