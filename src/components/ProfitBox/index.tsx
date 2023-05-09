f4c1569229dd0cb69635082ddafdf28b4c3eb529
webgently <oleksandrshkabura811@gmail.com>
2023-05-09 17:13:41 +0300

    update: profit component

--- src/components/ProfitBox/index.tsx
+++ src/components/ProfitBox/index.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import './profitbox.scss';
 
-const ProfitBox =({item, ind, profitCalcList, currentProfit}:any)=> {
-    return (
-        <div className={`history-item ${currentProfit === ind && 'history-item_active'}`}>
-            <span
-                className={
-                    profitCalcList[ind] >= 10
-                    ? 'text-yellow'
-                    : profitCalcList[ind] >= 2
-                    ? 'text-green'
-                    : 'text-blue'
-                }
-            >{`X ${item.value}`}</span>
-        </div>
-    )
-}
+const ProfitBox = ({ item, ind, playStatus, turboMode, profitCalcList, currentProfit }: any) => {
+  return (
+    <div
+      className={`history-item 
+        ${
+          currentProfit === ind &&
+          (turboMode || playStatus) &&
+          `history-item_active ${
+            profitCalcList[ind] >= 10
+              ? 'bg-yellow before:border-yellow after:border-yellow'
+              : profitCalcList[ind] >= 2
+              ? 'bg-green before:border-green after:border-green'
+              : 'bg-blue before:border-blue after:border-blue'
+          }`
+        }`}
+    >
+      <span
+        className={profitCalcList[ind] >= 10 ? 'text-yellow' : profitCalcList[ind] >= 2 ? 'text-green' : 'text-blue'}
+      >{`X ${item.value}`}</span>
+    </div>
+  );
+};
 
 export default ProfitBox;
