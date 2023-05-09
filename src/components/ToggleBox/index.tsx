63b27f062b00d13b1d011e55709fe3ed8711ef9a
webgently <oleksandrshkabura811@gmail.com>
2023-05-09 12:43:29 +0300

    update: toggle box component

--- src/components/ToggleBox/index.tsx
+++ src/components/ToggleBox/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './togglebox.scss';
 
-const ToggleBox = ({ value, setValue }: any) => {
+const ToggleBox = ({ value, setValue, playStatus }: any) => {
   return (
-    <div className={`toggle-box ${value && 'toggle-box-active'}`} onClick={() => setValue((prev: boolean) => !prev)}>
+    <div
+      className={`toggle-box ${value && 'toggle-box_active'} ${
+        playStatus ? 'pointer-events-none' : 'toggle-box_hover'
+      }`}
+      onClick={() => setValue((prev: boolean) => !prev)}
+    >
       <div className="toggle-box-inner">
-        <div className="toggle-box-btn"></div>
+        <div className="toggle-box-btn" />
       </div>
     </div>
   );
