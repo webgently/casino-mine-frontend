99e8af55e2c57308f51930ea1986cd481c791501
webgently <oleksandrshkabura811@gmail.com>
2023-05-09 12:39:43 +0300

    dep: tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,6 +27,7 @@ module.exports = {
       xs: '480px',
       sm: '640px',
       md: '768px',
+      slg: '964px',
       lg: '1024px',
       xl: '1280px',
       '2xl': '1536px',
@@ -36,7 +37,8 @@ module.exports = {
     extend: {
       colors: {
         yellow: '#ffc020',
-        blue: '#0094ff'
+        blue: '#0094ff',
+        green: '#0f6'
       },
       container: {
         center: true
