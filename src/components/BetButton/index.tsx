bbb61b7666ea4fd9d8866c856c67bde906ae1d84
webgently <oleksandrshkabura811@gmail.com>
2023-05-10 11:57:47 +0300

    feat: add mobile bet button

--- src/components/BetButton/index.tsx
+++ src/components/BetButton/index.tsx
@@ -3,37 +3,73 @@ import CricketImg from '../../assets/images/cricket.png';
 import CricketShadowImg from '../../assets/images/shadow.webp';
 import './betbutton.scss';
 
-const BetButton = ({ profitAmount, cardLoading, loading, type, turboMode }: any) => {
+const BetButton = ({ profitAmount, cardLoading, loading, type, turboMode, isMobile }: any) => {
   return (
-    <div className={`bet-button-${type} ${(loading || cardLoading) && 'pointer-events-none'}`}>
-      <div className="bet-button-inner">
-        <div className="bet-button-content">
-          <div className="bet-button-text">
-            {loading ? (
-              <div className="loading-container">
-                <img src={CricketShadowImg} alt="shadow" />
-                <div className="btn-loading">
-                  <img src={CricketImg} alt="loading..." />
-                </div>
-              </div>
-            ) : type === 'cancel' ? (
-              'Cancel'
-            ) : type === 'cashOut' ? (
-              'Cash Out'
-            ) : (
-              `${turboMode ? 'Place bet' : 'Start Game'}`
-            )}
-          </div>
-          {type === 'cashOut' && (
-            <div className="bet-button-sum">
-              <div className="bet-button-sum-content">
-                <span>${profitAmount}</span>
+    <>
+      {isMobile ? (
+        <div className={`mobile-bet-button ${(loading || cardLoading) && 'pointer-events-none'}`}>
+          <div className={`portrait-container _${type}`}>
+            <div className="portrait-inner">
+              <div className="portrait-content">
+                {loading ? (
+                  <div className="loading-container">
+                    <img src={CricketShadowImg} alt="shadow" />
+                    <div className="btn-loading">
+                      <img src={CricketImg} alt="loading..." />
+                    </div>
+                  </div>
+                ) : (
+                  <>
+                    {type === 'cancel' ? (
+                      <span className="bet-button-text">Cancel</span>
+                    ) : type === 'cashOut' ? (
+                      <span className="bet-button-text">Cash Out</span>
+                    ) : (
+                      <span className="bet-button-text">{turboMode ? 'Place bet' : 'Start Game'}</span>
+                    )}
+                    {type === 'cashOut' && (
+                      <span className="bet-button-sum">
+                        <span>${profitAmount}</span>
+                      </span>
+                    )}
+                  </>
+                )}
               </div>
             </div>
-          )}
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <div className={`bet-button-${type} ${(loading || cardLoading) && 'pointer-events-none'}`}>
+          <div className="bet-button-inner">
+            <div className="bet-button-content">
+              <div className="bet-button-text">
+                {loading ? (
+                  <div className="loading-container">
+                    <img src={CricketShadowImg} alt="shadow" />
+                    <div className="btn-loading">
+                      <img src={CricketImg} alt="loading..." />
+                    </div>
+                  </div>
+                ) : type === 'cancel' ? (
+                  'Cancel'
+                ) : type === 'cashOut' ? (
+                  'Cash Out'
+                ) : (
+                  `${turboMode ? 'Place bet' : 'Start Game'}`
+                )}
+              </div>
+              {type === 'cashOut' && (
+                <div className="bet-button-sum">
+                  <div className="bet-button-sum-content">
+                    <span>${profitAmount}</span>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
