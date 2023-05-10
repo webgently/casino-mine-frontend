675f9a1a0b48923b1389105785cbab643ad4b5ae
webgently <oleksandrshkabura811@gmail.com>
2023-05-10 11:55:53 +0300

    feat: add mobile togglebox

--- src/components/ToggleBox/index.tsx
+++ src/components/ToggleBox/index.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import './togglebox.scss';
 
-const ToggleBox = ({ value, setValue, playStatus }: any) => {
+const ToggleBox = ({ value, setValue, playStatus, isMobile }: any) => {
   return (
-    <div
-      className={`toggle-box ${value && 'toggle-box_active'} ${
-        playStatus ? 'pointer-events-none' : 'toggle-box_hover'
-      }`}
-      onClick={() => setValue((prev: boolean) => !prev)}
-    >
-      <div className="toggle-box-inner">
-        <div className="toggle-box-btn" />
-      </div>
-    </div>
+    <>
+      {isMobile ? (
+        <div
+          className={`mobile-toggle-box ${playStatus && 'pointer-events-none'}`}
+          onClick={() => setValue((prev: boolean) => !prev)}
+        >
+          <div className={`mobile-toggle-box-container ${value && '_active'}`}>
+            <div className="mobile-toggle-box-inner">
+              <div className="mobile-toggle-box-title">Turbo</div>
+              <div className="mobile-toggle-button" />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`toggle-box ${value && 'toggle-box_active'} ${playStatus && 'pointer-events-none'}`}
+          onClick={() => setValue((prev: boolean) => !prev)}
+        >
+          <div className="toggle-box-inner">
+            <div className="toggle-box-btn" />
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
