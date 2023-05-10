cc47a89a0bef30373065e29747e9e20dcfd392b4
webgently <oleksandrshkabura811@gmail.com>
2023-05-10 11:56:26 +0300

    feat: add mobile minebox

--- src/components/MineBox/index.tsx
+++ src/components/MineBox/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './minebox.scss';
 
-const NumberBox = ({ min, max, value, setValue, playStatus }: any) => {
+const NumberBox = ({ min, max, value, setValue, playStatus, isMobile }: any) => {
   const dec = () => {
     if (value <= min) return;
     setValue(Number(value) - 1);
@@ -19,23 +19,63 @@ const NumberBox = ({ min, max, value, setValue, playStatus }: any) => {
   };
 
   return (
-    <div className={`number-box ${playStatus && 'pointer-events-none'}`}>
-      <div className="number-box-inner">
-        <div className={`number-box-control-btn ${!playStatus && '_active'}`} onClick={dec}>
-          <span>-</span>
+    <>
+      {isMobile ? (
+        <div className={`mobile-mine-box ${playStatus && 'pointer-events-none'}`}>
+          <div className="mobile-mine-btns">
+            <div className="mobile-mine-btns-inner">
+              <div className="wicket-btn" onClick={() => onChange(5)}>
+                5
+              </div>
+              <div className="wicket-btn" onClick={() => onChange(7)}>
+                7
+              </div>
+              <div className="wicket-btn" onClick={() => onChange(10)}>
+                10
+              </div>
+              <div className="wicket-btn" onClick={() => onChange(12)}>
+                12
+              </div>
+            </div>
+          </div>
+          <div className="mobile-mine-input">
+            <div className="input-btn">
+              <span className="input-btn-inner" onClick={dec}>
+                -
+              </span>
+            </div>
+            <input
+              type="number"
+              value={value}
+              onChange={(e: any) => onChange(e.target.value)}
+              disabled={playStatus}
+              className={playStatus ? '!text-white/[0.5]' : 'text-white'}
+            />
+            <div className="input-btn" onClick={inc}>
+              <span className="input-btn-inner">+</span>
+            </div>
+          </div>
         </div>
-        <input
-          type="number"
-          value={value}
-          onChange={(e: any) => onChange(e.target.value)}
-          disabled={playStatus}
-          className={playStatus ? '!text-white/[0.5]' : 'text-white'}
-        />
-        <div className={`number-box-control-btn ${!playStatus && '_active'}`} onClick={inc}>
-          <span>+</span>
+      ) : (
+        <div className={`mine-box ${playStatus && 'pointer-events-none'}`}>
+          <div className="mine-box-inner">
+            <div className={`mine-box-control-btn ${!playStatus && '_active'}`} onClick={dec}>
+              <span>-</span>
+            </div>
+            <input
+              type="number"
+              value={value}
+              onChange={(e: any) => onChange(e.target.value)}
+              disabled={playStatus}
+              className={playStatus ? '!text-white/[0.5]' : 'text-white'}
+            />
+            <div className={`mine-box-control-btn ${!playStatus && '_active'}`} onClick={inc}>
+              <span>+</span>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
